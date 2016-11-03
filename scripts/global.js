b59b8a4d027082d03736f8a6f74bd596a46fd32e
jian.wu <376773242@qq.com>
2016-11-03 16:37:50 +0800

    javascript图片库

--- scripts/global.js
+++ scripts/global.js
@@ -174,6 +174,62 @@ function prepareInternalnav() {
     }
 }
 
+function showPic(whichpic) {
+    if (!document.getElementById("placeholder")) return true;
+    var source = whichpic.getAttribute("href");
+    var placeholder = document.getElementById("placeholder");
+    placeholder.setAttribute("src", source);
+    if (!document.getElementById("description")) return false;
+    var text;
+    if (whichpic.getAttribute("title")) {
+        text = whichpic.getAttribute("title");
+    } else {
+        text = "";
+    }
+    var description = document.getElementById("description");
+    if (description.firstChild.nodeType == 3) {//文本节点
+        description.firstChild.nodeValue = text;
+    }
+    return false;
+}
+
+function preparePlaceholder() {
+    if (!document.createElement) return false;
+    if (!document.createTextNode) return false;
+    if (!document.getElementById) return false;
+    if (!document.getElementById("imagegallery")) return false;
+
+    var placeholder = document.createElement("img");
+    placeholder.setAttribute("id", "placeholder");
+    placeholder.setAttribute("src", "images/placeholder.gif");
+    placeholder.setAttribute("alt", "my image gallery");
+
+    var description = document.createElement("p");
+    description.setAttribute("id", "description");
+
+    var descText = document.createTextNode("choose an image");
+    description.appendChild(descText);
+
+    var gallery = document.getElementById("imagegallery");
+    insertAfter(description, gallery);
+    insertAfter(placeholder, description);
+}
+
+function prepareGallery() {
+    if (!document.getElementsByTagName) return false;
+    if (!document.getElementById) return false;
+    if (!document.getElementById("imagegallery")) return false;
+    var gallery = document.getElementById("imagegallery");
+    var links = gallery.getElementsByTagName("a");
+    for (var i = 0; i < links.length; i++) {
+        links[i].onclick = function () {
+            return showPic(this);
+        }
+    }
+}
+
 addLoadEvent(highlightPage);
 addLoadEvent(prepareSlideshow);
 addLoadEvent(prepareInternalnav);
+addLoadEvent(preparePlaceholder);
+addLoadEvent(prepareGallery);
